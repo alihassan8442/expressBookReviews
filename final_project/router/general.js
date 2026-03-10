99b1faaf18587acd1edce2afbef5586c5402ec50
alihassan8442 <alihassan07356@gmail.com>
2026-03-10 19:16:11 -0400

    completed task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,10 +39,23 @@ public_users.get('/', function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async (req, res) => {
     const isbn = req.params.isbn;
-    const book = books[isbn];
-    return res.send(JSON.stringify(book, null, 4));
+
+    try {
+        const getBookByISBN = (isbn) => {
+            return new Promise((resolve, reject) => {
+                const book = books[isbn];
+                if (book) resolve(book);
+                else reject("Book not found");
+            });
+        };
+
+        const book = await getBookByISBN(isbn);
+        res.status(200).send(JSON.stringify(book, null, 4));
+    } catch (error) {
+        res.status(404).json({ message: error });
+    }
 });
   
 // Get book details based on author
